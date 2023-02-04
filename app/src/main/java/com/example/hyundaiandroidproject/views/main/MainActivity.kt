dd6a79e6e42d0425d2bcfae6b3019945f9e4a5eb
skylee273 <skylee273@koreatech.ac.kr>
2023-02-04 18:36:58 +0900

    Feat: MainActivity.kt 뷰 연결
    MainActivity.kt View 연결
     - ClickListener 생성
     - 뷰 연결

--- app/src/main/java/com/example/hyundaiandroidproject/views/main/MainActivity.kt
+++ app/src/main/java/com/example/hyundaiandroidproject/views/main/MainActivity.kt
@@ -1,4 +1,30 @@
 package com.example.hyundaiandroidproject.views.main
 
-class MainActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.hyundaiandroidproject.R
+import com.example.hyundaiandroidproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnActivityMainSearch.setOnClickListener(this)
+
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0?.id){
+            R.id.btnActivityMainSearch -> {
+                // startActivity(Intent(this@MainActivity, SearchActivity::class.java))
+            }
+        }
+    }
+
 }
