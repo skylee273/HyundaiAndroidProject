318dcf29a41649453f6d5b4e9ca21c3fc3e8ddce
skylee273 <skylee273@koreatech.ac.kr>
2023-02-05 04:08:30 +0900

    Feat: SearchActivity.kt 생성
    SearchActivity.kt 생성
     - menuItem 설정
     - menuSearch 설정

--- app/src/main/java/com/example/hyundaiandroidproject/views/search/SearchActivity.kt
+++ app/src/main/java/com/example/hyundaiandroidproject/views/search/SearchActivity.kt
@@ -1,4 +1,104 @@
 package com.example.hyundaiandroidproject.views.search
 
-class SearchActivity {
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.SearchView
+import com.example.hyundaiandroidproject.R
+import com.example.hyundaiandroidproject.base.BaseActivity
+import com.example.hyundaiandroidproject.databinding.ActivitySearchBinding
+import com.jakewharton.rxbinding4.widget.queryTextChangeEvents
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+
+
+class SearchActivity : BaseActivity<ActivitySearchBinding>(R.layout.activity_search) {
+
+    private lateinit var menuSearch: MenuItem
+    private lateinit var searchView: SearchView
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_activity_search, menu)
+        menuSearch = menu.findItem(R.id.menu_activity_search_query)
+
+        searchView = (menuSearch.actionView as SearchView)
+        searchView.queryTextChangeEvents()
+            .filter { it.isSubmitted }
+            .map { it.queryText }
+            .map { it.toString() }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { query ->
+                updateTitle(query)
+                hideSoftKeyboard()
+                collapseSearchView()
+                // searchRepository(query)
+            }
+
+        with(menuSearch) {
+            setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
+                override fun onMenuItemActionExpand(p0: MenuItem): Boolean {
+                    return true
+                }
+
+                override fun onMenuItemActionCollapse(p0: MenuItem): Boolean {
+                    if ("" == searchView.query) {
+                        finish()
+                    }
+                    return true
+                }
+            })
+        }
+        menuSearch.expandActionView()
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (R.id.menu_activity_search_query == item.itemId) {
+            item.expandActionView()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    private fun updateTitle(query: String) {
+        // 별도의 변수 선언 없이
+        // getSupportActionBar()의 반환값이 널이 아닌 경우에만 작업을 수행한다.
+        supportActionBar?.run {
+            subtitle = query
+        }
+    }
+
+    private fun hideSoftKeyboard() {
+        // 별도의 변수 선언 없이 획득한 인스턴스의 범위 내에서 작업을 수행한다.
+        (getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager).run {
+            hideSoftInputFromWindow(searchView!!.windowToken, 0)
+        }
+    }
+
+    private fun collapseSearchView() {
+        menuSearch.collapseActionView()
+    }
+
+    private fun showProgress() {
+        binding.pbActivitySearch.visibility = View.VISIBLE
+    }
+
+    private fun hideProgress() {
+        binding.pbActivitySearch.visibility = View.GONE
+    }
+
+    private fun showError(message: String) {
+        with(binding.tvActivitySearchMessage) {
+            text = message ?: "Unexpected error."
+            visibility = View.VISIBLE
+        }
+    }
+
+    private fun hideError() {
+        with(binding.tvActivitySearchMessage) {
+            text = ""
+            visibility = View.GONE
+        }
+    }
 }
