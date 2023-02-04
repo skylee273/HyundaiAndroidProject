6a909f2ac48d16fa2858fd065f2291249b986fdf
skylee273 <skylee273@koreatech.ac.kr>
2023-02-04 18:23:30 +0900

    Feat: 신규 베이스 Activity 설정
    신규 BaseActivity 설정

---
+++ app/src/main/java/com/example/hyundaiandroidproject/base/BaseActivity.kt
@@ -0,0 +1,21 @@
+package com.example.hyundaiandroidproject.base
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<B : ViewDataBinding>(
+    @LayoutRes val layoutId: Int
+) : AppCompatActivity() {
+
+    lateinit var binding : B
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        binding.lifecycleOwner = this
+    }
+}
