5def7b8e4c771ae8d2c9a99a16235e51736c638c
skylee273 <skylee273@koreatech.ac.kr>
2023-02-05 21:42:56 +0900

    Feat: SearchAdapter.kt 개선

--- app/src/main/java/com/example/hyundaiandroidproject/views/search/SearchViewModel.kt
+++ app/src/main/java/com/example/hyundaiandroidproject/views/search/SearchViewModel.kt
@@ -2,9 +2,6 @@ package com.example.hyundaiandroidproject.views.search
 
 import android.annotation.SuppressLint
 import android.app.Application
-import android.view.View
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.hyundaiandroidproject.api.model.MovieEntity
 import com.example.hyundaiandroidproject.api.provideApi
@@ -12,22 +9,18 @@ import com.example.hyundaiandroidproject.base.BaseViewModel
 import com.example.hyundaiandroidproject.extentsion.plusAssign
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Single
-import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import java.lang.IllegalStateException
 
 class SearchViewModel(application: Application) : BaseViewModel(application) {
 
-    private val _serverMessage = MutableLiveData<MessageSet>()
     private val _movieList = MutableLiveData<MutableList<MovieEntity>?>()
+    val movieList: MutableLiveData<MutableList<MovieEntity>?> get() = _movieList
 
     private val api by lazy {
         provideApi()
     }
-    val serverMessage: LiveData<MessageSet> get() = _serverMessage
-    val movieList: MutableLiveData<MutableList<MovieEntity>?> get() = _movieList
 
-    @SuppressLint("NotifyDataSetChanged")
     fun searchMovie(query: String) {
         compositeDisposable += api.getSearchMovie(query)
             .flatMap {
@@ -42,24 +35,16 @@ class SearchViewModel(application: Application) : BaseViewModel(application) {
             .doOnSubscribe {
                 showProgress()
                 hideError()
-                _serverMessage.value = MessageSet.LOADING
             }
             .doOnTerminate {
                 hideProgress()
             }
             .subscribe({ items ->
                 _movieList.value = items.toMutableList()
-                _serverMessage.value = MessageSet.SUCCESS
 
             }) {
                 showError(it.message!!)
             }
-    }
-
-    enum class MessageSet {
-        ERROR,
-        SUCCESS,
-        LOADING
     }
 
 }
